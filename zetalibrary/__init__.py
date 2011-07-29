f5ff05dea9dcd5ef2eb6e78bbedaeabcb308e205
Kirill Klenov <horneds@gmail.com>
2011-07-29 15:23:38 +0400

    Follow on python-scss.

--- zetalibrary/__init__.py
+++ zetalibrary/__init__.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 import os.path
 
-version_info = (0, 4, 22)
+version_info = (0, 4, 23)
 
 __version__ = VERSION = '.'.join(map(str, version_info))
 __project__ = PROJECT = __name__
